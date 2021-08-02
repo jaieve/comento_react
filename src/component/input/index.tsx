e119fcb0c51dbc8c9e044da6097179e9f27a621a
jaieve <yoori0303@naver.com>
2021-08-02 23:54:17 +0900

    글쓰기 기능 확인(required로만 null check)

--- src/component/input/index.tsx
+++ src/component/input/index.tsx
@@ -39,14 +39,25 @@ const Input = () => {
   return (
     <div>
       <form>
+        <h3>글쓰기 폼</h3>
+        <label>title</label>
         <input id="title" value={request.title} onChange={handleChange}></input>
         <br />
+        <label>writer</label>
+        <input
+          id="writer"
+          value={request.writer}
+          onChange={handleChange}
+        ></input>
+        <br />
+        <label>content</label>
         <input
           id="content"
           value={request.content}
           onChange={handleChange}
         ></input>
-        <button onClick={writePost}></button>
+        <br />
+        <button onClick={writePost}>글 등록</button>
       </form>
     </div>
   );
